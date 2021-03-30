1287744a0cf41e928e5b4a156e5641f41bdbd2a9
Till Hainbach <Till.Hainbach@posteo.de>
2021-03-30 18:10:00 +0200

    chore(stylelint): add webpack stylelint support

---
+++ gatsby-node.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+const StylelintBarePlugin = require('stylelint-bare-webpack-plugin');
+
+const defaultOptions = {
+  files: '**/*.{js,jsx}',
+  emitErrors: false,
+};
+
+exports.onCreateWebpackConfig = ({ actions }, { plugins, ...options }) => {
+  actions.setWebpackConfig({
+    plugins: [new StylelintBarePlugin({ ...defaultOptions, ...options })],
+  });
+};
